ad6b1fcc6ab04e526341660fecdfb5a5bed68287
antonarhipov <ant.arhipov@gmail.com>
2024-12-16 09:25:01 +0200

    Day16: learning.md

--- Sources/Learning/main.swift
+++ Sources/Learning/main.swift
@@ -1,26 +1,11 @@
 import Foundation
 
-enum MyErrror: Error {
-    case invalidInput
-    case invalidOutput
-}
-
-func functionThatThrows() throws -> Int {
-
-    let random = Int.random(in: 0...10)
-    if random < 10 {
-        throw MyErrror.invalidOutput
+struct MyStruct {
+    let lambda: (Int) -> Bool
+    
+    
+    init(lambda: @escaping (Int) -> Bool) {
+        self.lambda = lambda
     }
-    return random
 }
-
-
-
-if let number = try? functionThatThrows() {
-    print("Number: \(number)")
-} else {
-    print("Error")
-}
-
-
 
